d7fb80ec4e0c0737791934c5de43fb71e542320c
Aryangurung1 <aryan.gurung683@gmail.com>
2025-07-15 13:31:32 +0545

    fix: this one

--- locals.tf
+++ locals.tf
@@ -1,9 +1,11 @@
 locals {
-  ami_id = var.ami_ids[var.environment]
+  ami_id         = var.ami_ids[var.environment]
+  instance_name  = "aryan-${var.environment}-ec2"
 
   tags = merge(
     var.common_tags,
     {
+      Name        = local.instance_name
       Environment = var.environment
     }
   )
